c800d9d15086fb36c85231aeb734672729834369
Jeroen van Daelen <74192811+JvanDaelen@users.noreply.github.com>
2024-06-30 15:44:20 +0200

    Added some example terrain figures

--- map_generator.py
+++ map_generator.py
@@ -1,5 +1,6 @@
 ### Code to generate a numpy array based map containing randomly generated objects
 import numpy as np
+import matplotlib.pyplot as plt
 
 class Map():
     #Grid label constants
@@ -13,11 +14,11 @@ class Map():
     EXPLORED = 1
 
     def __init__(self, 
-                 grid_width = 10, 
-                 grid_height = 10, 
-                 nr_cliffs = 3, 
-                 cliff_length_max = 4,
-                 cliff_length_min = 2
+                 grid_width = 30, 
+                 grid_height = 30, 
+                 nr_cliffs = 6, 
+                 cliff_length_max = 15,
+                 cliff_length_min = 6
                  ) -> None:
         # make class vars
         self.grid_width = grid_width
@@ -26,13 +27,10 @@ class Map():
         self.cliff_length_max = cliff_length_max
         self.cliff_length_min = cliff_length_min
         self.terrain = np.zeros((grid_width, grid_height))
-        print(self.terrain)
         self._generate_cliffs()
-        print(self.terrain)
     
     def _generate_cliffs(self):
         for cliff_nr in range(self.nr_cliffs):
-            print(f'{cliff_nr = }')
             # Pick random point
             center_x, center_y = self.get_random_coordinate()
             # self.terrain[center_x, center_y] = self.TERRAIN_TYPES['CLIFF']
@@ -49,9 +47,7 @@ class Map():
             x_walk = np.cos(np.deg2rad(direction_deg))
             y_walk = np.sin(np.deg2rad(direction_deg))
             for step_nr in range(int(np.ceil(random_length/2)) + 1):
-                print(f'{step_nr = }')
                 if 0 <= center_x + x_walk * step_nr < self.grid_width and 0 <= center_y + y_walk * step_nr < self.grid_height:
-                    print(f'{center_x + x_walk * step_nr = }')
                     self.terrain[int(center_x + x_walk * step_nr), int(center_y + y_walk * step_nr)] = self.TERRAIN_TYPES['CLIFF']
                 if 0 <= center_x - x_walk * step_nr < self.grid_width and 0 <= center_y - y_walk * step_nr < self.grid_height:
                     self.terrain[int(center_x - x_walk * step_nr), int(center_y - y_walk * step_nr)] = self.TERRAIN_TYPES['CLIFF']
@@ -63,4 +59,10 @@ class Map():
 
 
 if __name__ == '__main__':
-    map = Map()
+    for i in range(20):
+        map = Map()
+        fig, ax = plt.subplots(figsize=(6,6))
+        plt.imshow(map.terrain, cmap="binary")
+        # plt.axis('off')
+        plt.grid(True)
+        plt.savefig(f'figures/test_figures/cliff-{i}')
